918998c1ef038f0b684a9f756170f83d0568c49e
Dennis Cual <dennis.cual@ombori.com>
2018-11-15 16:13:21 +0800

    setup eslint and some package.json scripts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 // If we gonna create a named function, we gonna use function declaration syntax.
 // Then if we gonna create an anonymous function, go to flat arrow syntax.
 
+// TODO: Add linting. Either eslint or standardjs.
 function Counter() {
   const [ counter, setCounter ] = useState(0);
 
