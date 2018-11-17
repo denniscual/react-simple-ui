eb342c459302e16368d203be1641c797b012720e
Dennis Cual <dennis.cual@ombori.com>
2018-11-18 00:03:25 +0800

    add prettier and eslint react-hooks config

--- src/App.js
+++ src/App.js
@@ -32,11 +32,11 @@ import styled from 'styled-components'
 // Helpers
 // It returns the previous version of a value.
 function usePrevious(value) {
-  const ref = useRef();
+  const ref = useRef()
   useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
+    ref.current = value
+  })
+  return ref.current
 }
 
 // Styles
@@ -46,29 +46,25 @@ const sc = {
   tabs: styled.ul`
     margin: 0;
   `,
-  tabsHeader: styled.header`
-  `,
+  tabsHeader: styled.header``,
   tabItem: styled.li`
-    display: ${({ active }) => active ? 'block' : 'none'};
+    display: ${({ active }) => (active ? 'block' : 'none')};
     list-style-type: none;
   `,
 }
 
-function TabItem (
-  {
-    children,
-    title,
-    active,
-  }:
-  {
-    children: Element<any> | any,
-    title: string,
-    active: boolean,
-  }
-): Element<'li'> {
+function TabItem({
+  children,
+  title,
+  active,
+}: {
+  children: Element<any> | any,
+  title: string,
+  active: boolean,
+}): Element<'li'> {
   return (
     <sc.tabItem active={active}>
-      <div>{ children }</div>
+      <div>{children}</div>
     </sc.tabItem>
   )
 }
@@ -80,22 +76,25 @@ TabItem.defaultProps = {
 // TODO: Need to accept some event handlers. This handlers gets invoke when there is changes in active Tab and there is selection.
 // TODO: Make this component more reusable and add some base styles.
 // TODO: Add optional lazy load feature to the content of Tab items using lazy and Suspense API's.
-function Tabs(
-  {
-    children,
-    activeIndex,
-  }: {
-    children: ChildrenArray<Element<typeof TabItem>>,
-    activeIndex: number,
-  }
-): Element<'ul'> {
-  const [ active, setActive ] = useState(activeIndex)
+function Tabs({
+  children,
+  activeIndex,
+}: {
+  children: ChildrenArray<Element<typeof TabItem>>,
+  activeIndex: number,
+}): Element<'ul'> {
+  const [active, setActive] = useState(activeIndex)
+  const prevActive = usePrevious(active)
+  // TODO: Need to learn why it stores the previous state.
+  console.log('prevActive', prevActive)
+  console.log('nextActive', active)
+  console.log('isEqual', prevActive === active)
   // create the tabsHeader. Note that the computation has been memoized. Means if no
   // changes in children, it returns the cache value. No re-rendering happens.
-  const tabsHeader = useMemo(() => {
-    // create an array whose elements are the title prop of every child. Memoized the return value.
-    const tabItemHeaders = Children
-      .map(children, (child) => {
+  const tabsHeader = useMemo(
+    () => {
+      // create an array whose elements are the title prop of every child. Memoized the return value.
+      const tabItemHeaders = Children.map(children, child => {
         const isValid = isValidElement(child)
         // If valid.
         if (isValid) {
@@ -103,22 +102,30 @@ function Tabs(
         }
         // Else.
         throw new TypeError('Value is not valid element.')
-      })
-      .map((title, i) => <button key={i} onClick={() => setActive(i)}>{ title }</button>)
-    return <sc.tabsHeader>{ tabItemHeaders }</sc.tabsHeader>
-  }, [ children ])
+      }).map((title, i) => (
+        <button key={i} onClick={() => setActive(i)}>
+          {title}
+        </button>
+      ))
+      return <sc.tabsHeader>{tabItemHeaders}</sc.tabsHeader>
+    },
+    [children],
+  )
   // Create body/content of the Tabs. Value has been memoized.
-  const tabsBody = useMemo(() => {
-    const updatedTabItemBasedInIndex = cloneElement(
-      Children.toArray(children)[active],
-      { active: true }
-    )
-    return <div>{ update(active, updatedTabItemBasedInIndex, children) }</div>
-  }, [ children, active ])
+  const tabsBody = useMemo(
+    () => {
+      const updatedTabItemBasedInIndex = cloneElement(
+        Children.toArray(children)[active],
+        { active: true },
+      )
+      return <div>{update(active, updatedTabItemBasedInIndex, children)}</div>
+    },
+    [children, active],
+  )
   return (
     <sc.tabs>
-      { tabsHeader }
-      { tabsBody }
+      {tabsHeader}
+      {tabsBody}
     </sc.tabs>
   )
 }
@@ -141,29 +148,29 @@ function App() {
         <div>This is the Tab 3</div>
       </TabItem>
     </Tabs>
-  );
+  )
 }
 
-export default App;
+export default App
 
 // Counter App using React hooks
-function Counter() {
-  // If we are using useState, basically our Component became 'stateful'.
-  const [ counter, setCounter ] = useState(0);
-
-  function handleDecrement() {
-    setCounter(prev => prev - 1);
-  }
-
-  function handleIncrement() {
-    setCounter(prev => prev + 1);
-  };
-
-  return (
-    <div>
-      <button onClick={handleDecrement}>Decrement</button>
-      <div>{counter}</div>
-      <button onClick={handleIncrement}>Increment</button>
-    </div>
-  );
-}
+/* function Counter() {
+ *   // If we are using useState, basically our Component became 'stateful'.
+ *   const [counter, setCounter] = useState(0)
+ *
+ *   function handleDecrement() {
+ *     setCounter(prev => prev - 1)
+ *   }
+ *
+ *   function handleIncrement() {
+ *     setCounter(prev => prev + 1)
+ *   }
+ *
+ *   return (
+ *     <div>
+ *       <button onClick={handleDecrement}>Decrement</button>
+ *       <div>{counter}</div>
+ *       <button onClick={handleIncrement}>Increment</button>
+ *     </div>
+ *   )
+ * } */
